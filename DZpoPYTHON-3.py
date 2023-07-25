efe461b4c9b5bcd169d5aabc854ff22f9abf6e2f
Konstantin <pkv90@yandex.ru>
2023-07-26 01:21:11 +0500

    1

---
+++ DZpoPYTHON-3.py
@@ -0,0 +1,7 @@
+# Task1
+current = int(input('Введите номер вагона по порядку следования: '))
+vagon = int (input('Витя сел в вагон номер: '))
+if current == vagon:
+    print ('Не хватает информации')
+else:
+    print(f'Количество вагонов = {current + vagon}')
